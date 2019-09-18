4de460a0aacb72cbae2b9cb98fc68bf63b5fac5c
Moahammad Almajarish <55390435+mohammadalmajarish@users.noreply.github.com>
2019-09-18 14:39:49 +0400

    Update script.js

--- FrontEnd September Competition/assets/js/script.js
+++ FrontEnd September Competition/assets/js/script.js
@@ -2,15 +2,16 @@ $(document).ready(() => {
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
     const loginBtn = $('#login-btn');
+    const forgetBtn = $('#forget-password');
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000
+    });
 
     loginBtn.click(() => {
         if (!emailInput.value || !passwordInput.value) {
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000
-            });
             Toast.fire({
                 type: 'error',
                 title: 'Sorry, but you forgot something!'
@@ -27,6 +28,12 @@ $(document).ready(() => {
             type: 'success',
             title: 'Logged in successfully! Welcome {user}' // {user} can be replaced by using .replace method
         });
+    });
+    forgetBtn.click(() => {
+        Toast.fire({
+            type: 'error',
+            title: 'Remember it! That\'s not my business'
+        })
     });
     console.log('Hello World!');
     'Just for copyright. xD'; // Made by 14 years old boii | Mohammad Almajarish.
